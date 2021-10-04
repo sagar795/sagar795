1ce25532d88eec07f080d563bb0d259ff8d7980b
sagar795 <91180135+sagar795@users.noreply.github.com>
2021-10-04 13:11:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,4 +6,18 @@ apt install -y  mariadb-server mariadb-client
 service mariadb start
 apt install -y  php libapache2-mod-php php-mysql
 apt install -y  php-redis php-zip
-service apache2 restart
+cp  /info.php /var/www/html/info.php
+apt-get install wget -y
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+cp -R wordpress /var/www/html/mysite.com
+chown -R www-data:www-data /var/www/html/mysite.com
+chmod -R 775 /var/www/html/mysite.com
+service mysql start
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+mysql -uroot <<MYSQL_SCRIPT
+CREATE DATABASE mysite;
+CREATE USER 'mysiteadmin'@'%' IDENTIFIED BY '123s';
+GRANT ALL PRIVILEGES ON mysite.* TO 'mysiteadmin'@'%';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
