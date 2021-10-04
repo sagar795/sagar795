86573b5ffb7d50dee2e4b7614b7c6206543ec543
sagar795 <91180135+sagar795@users.noreply.github.com>
2021-10-04 13:00:59 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,6 +3,6 @@ service mysql start
 mysql -uroot <<MYSQL_SCRIPT
 CREATE DATABASE mysite;
 CREATE USER 'mysiteadmin'@'localhost' IDENTIFIED BY '123s';
-GRANT ALL PRIVILEGES ON mysite.* TO 'mysiteadmin'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'mysiteadmin'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
